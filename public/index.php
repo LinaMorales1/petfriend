27b11ed803f79efd8f3756f4f366e07411e39b7d
Lina <linamoralessandoval@gmail.com>
2025-07-16 21:14:00 -0500

    FUNCIONALIDAD

--- public/index.php
+++ public/index.php
@@ -1,9 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
 require_once '../core/App.php';
 require_once '../core/Controller.php';
 require_once '../core/Model.php';
